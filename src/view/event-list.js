bedbfbe1768a17c2cd7b3b62642ddf0f48de6fcf
Roman Vlasenko <klavionik@gmail.com>
2021-04-13 23:02:50 +0300

    Список точек маршрута выделен в компонент

---
+++ src/view/event-list.js
@@ -0,0 +1,29 @@
+import {createElement} from '../utils/common';
+
+const createEventListTemplate = () => {
+  return '<ul class="trip-events__list"></ul>';
+};
+
+class EventList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createEventListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default EventList;
