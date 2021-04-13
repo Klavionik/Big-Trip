c177c05520b66b474f9edf3f02ba1ca830fee6cd
Roman Vlasenko <klavionik@gmail.com>
2021-04-13 23:47:14 +0300

    Удален index.js из директории view

--- src/view/index.js
+++
@@ -1,7 +0,0 @@
-export * from './sorting';
-export * from './filters';
-export * from './menu';
-export * from './event-item-new';
-export * from './event-item';
-export * from './event-item-edit';
-export * from './trip-info';
