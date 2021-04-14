887a8442823a9622b755497ee0f180a053607e7f
Roman Vlasenko <klavionik@gmail.com>
2021-04-15 00:19:08 +0300

    Исправлено наименование импортов в main.js, исправлены названия модулей с формами

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import {generateEventItem, generateAvailableOffers} from './mock/event-item';
 import {calculateTripInfo} from './utils/trip-info';
 import MenuView from './view/menu';
 import SortingView from './view/sorting';
-import Filters from './view/filters';
-import TripInfo from './view/trip-info';
+import FiltersView from './view/filters';
+import TripInfoView from './view/trip-info';
 import EventItemView from './view/event-item';
-import EventEditFormView from './view/event-item-edit';
-import EventList from './view/event-list';
+import EventEditFormView from './view/event-edit-form';
+import EventListView from './view/event-list';
 import {render} from './utils/common';
 
 const EVENT_ITEMS_COUNT = 8;
@@ -43,10 +43,10 @@ const tripEventsElement = document.querySelector('.trip-events');
 render(navigationElement, new MenuView().getElement());
 
 const tripInfo = calculateTripInfo(eventItems.slice(1));
-render(tripMainElement, new TripInfo(tripInfo).getElement(), 'afterbegin');
-render(filtersElement, new Filters().getElement());
+render(tripMainElement, new TripInfoView(tripInfo).getElement(), 'afterbegin');
+render(filtersElement, new FiltersView().getElement());
 render(tripEventsElement, new SortingView().getElement());
-render(tripEventsElement, new EventList().getElement());
+render(tripEventsElement, new EventListView().getElement());
 
 const tripEventsListElement = document.querySelector('.trip-events__list');
 
