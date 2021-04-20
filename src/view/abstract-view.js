57719666ffca1923ed069f699aaff52a3b30f16e
Roman Vlasenko <klavionik@gmail.com>
2021-04-20 23:38:17 +0300

    Добавлен абстрактный класс компонентов

---
+++ src/view/abstract-view.js
@@ -0,0 +1,29 @@
+import {createElement} from '../utils/common';
+
+class AbstractView {
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Cannot create an instance of an abstract class.');
+    }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error('Method not implemented in the abstract class.');
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default AbstractView;
